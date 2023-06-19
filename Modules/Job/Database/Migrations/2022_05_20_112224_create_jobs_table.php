c55423a28b8902e8f4cc1aec8d2271340ea690b9
foisal_techvill <foisal.techvill@gmail.com>
2023-06-19 22:33:41 +0600

    User Query And Job Migration File Added

---
+++ Modules/Job/Database/Migrations/2022_05_20_112224_create_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('job_category_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('company')->nullable();
+            $table->string('location')->nullable();
+            $table->date('end_date');
+            $table->text('description')->nullable();
+            $table->string('file')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
