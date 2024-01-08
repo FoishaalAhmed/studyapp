ff7c7d1c0ba9dffc65367494415d27aef11a7605
Foisal Ahmed <foisal.cse@gmail.com>
2024-01-08 20:09:33 +0600

    Missing migration file added

---
+++ Modules/Testimonial/Database/Migrations/2024_01_08_140637_create_testimonials_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('position', 100);
+            $table->tinyInteger('star');
+            $table->text('message');
+            $table->string('photo')->nullable();
+            $table->bigInteger('user_id')->nullable();
+            $table->string('status', 15)->default('In Review')->comment('In Review, Published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};
