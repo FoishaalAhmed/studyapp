b5a975e364b45267e5955c1a849c04771c8f3966
Foisal Ahmed <foisal.cse@gmail.com>
2024-01-27 22:43:02 +0600

    User dashboard view reorganized

--- app/Http/Controllers/User/DashboardController.php
+++ app/Http/Controllers/User/DashboardController.php
@@ -3,14 +3,42 @@
 namespace App\Http\Controllers\User;
 
 use App\Models\Buy;
+use App\Enums\CategoryType;
 use Modules\Mcq\Entities\ModelMark;
 use App\Http\Controllers\Controller;
+use Modules\Category\Entities\{CategoryUser , SubCategory};
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('backend.user.dashboard');
+        $userCategories  = CategoryUser::where('user_id', auth()->id())->pluck('category_id')->toArray();
+
+        $data = [
+
+            'mcqCategories' => $this->getCategories(
+                'models', 
+                [CategoryType::ModelTest, CategoryType::CommonModelTest], 
+                $userCategories, 
+                'models_count'
+            ),
+
+            'ebookCategories' => $this->getCategories(
+                'ebooks', 
+                [CategoryType::Ebook, CategoryType::CommonEbook], 
+                $userCategories, 
+                'ebooks_count'
+            ),
+
+            'sheetCategories' => $this->getCategories(
+                'sheets', 
+                [CategoryType::LectureSheet, CategoryType::CommonLectureSheet], 
+                $userCategories, 
+                'sheets_count'
+            )
+        ];
+
+        return view('backend.user.dashboard', $data);
     }
 
     public function rank()
@@ -30,5 +58,10 @@ class DashboardController extends Controller
         ];
 
         return view('backend.user.resource-buy', $data);
+    }
+
+    private function getCategories($relation, $type, $categories, $relationCount)
+    {
+        return SubCategory::withCount($relation)->whereIn('type', $type)->whereIn('category_id', $categories)->latest($relationCount)->take(8)->get();
     }
 }
