0557c522eba638483a65ad8625b7bb0b35f35671
Foisal Ahmed <foisal.cse@gmail.com>
2024-02-14 21:04:34 +0600

    Api lecture sheets feature completed

--- Modules/LectureSheet/Http/Controllers/Api/SheetController.php
+++ Modules/LectureSheet/Http/Controllers/Api/SheetController.php
@@ -3,7 +3,62 @@
 namespace Modules\LectureSheet\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Enums\{CategoryType, Status};
+use Modules\LectureSheet\Entities\LectureSheet;
+use Modules\Category\Entities\{ChildCategory, SubCategory};
 
 class SheetController extends Controller
 {
+    public function index()
+    {
+        $sheets = LectureSheet::where('status', Status::PUBLISHED)->latest()->get(['id', 'chapter', 'thumb', 'type']);
+        return $this->successResponse($sheets);
+    }
+
+    public function category()
+    {
+        $categories = SubCategory::withCount('sheets')->where('type', CategoryType::CommonLectureSheet)->oldest('name')->get();
+        return $this->successResponse($categories);
+    }
+
+    public function premiumSubCategory($categoryId)
+    {
+        $categories = ChildCategory::withCount('sheets')->where(['type' => CategoryType::LectureSheet, 'sub_category_id' => $categoryId])->oldest('name')->get();
+        return $this->successResponse($categories);
+    }
+
+    public function subCategory($categoryId)
+    {
+        $categories = ChildCategory::withCount('sheets')->where('sub_category_id', $categoryId)->oldest('name')->get();
+        return $this->successResponse($categories);
+    }
+
+    public function categorySheet($categoryId)
+    {
+        $sheets = LectureSheet::where(['child_category_id' => $categoryId, 'status' => Status::PUBLISHED])->latest()->get(['id', 'chapter', 'thumb', 'type']);
+        return $this->successResponse($sheets);
+    }
+
+    public function subjectSheet($subjectId)
+    {
+        $sheets = LectureSheet::where(['subject_id' => $subjectId, 'status' => Status::PUBLISHED])->latest()->get(['id', 'chapter', 'thumb', 'type']);
+        return $this->successResponse($sheets);
+    }
+
+    public function categorySubjectSheet($categoryId, $subjectId)
+    {
+        $sheets = LectureSheet::where(['child_category_id' => $categoryId, 'subject_id' => $subjectId, 'status' => Status::PUBLISHED])->latest()->get(['id', 'chapter', 'thumb', 'type']);
+        return $this->successResponse($sheets);
+    }
+
+    public function detail($id)
+    {
+        $sheet = LectureSheet::where('id', $id)->first(['id', 'chapter', 'thumb', 'file', 'type']);
+
+        if (is_null($sheet)) {
+            return $this->unprocessableResponse([], __('Lecture sheet does not exist.'));
+        }
+
+        return $this->successResponse($sheet);
+    }
 }
--- Modules/LectureSheet/Routes/api.php
+++ Modules/LectureSheet/Routes/api.php
@@ -5,13 +5,13 @@ use Modules\LectureSheet\Http\Controllers\Api\SheetController;
 
 Route::group(['middleware' => 'auth:sanctum'], fn () => [
     Route::controller(SheetController::class)->group(fn () => [
-        Route::get('category-subject-sheets/{category_id}/{subject_id}', 'categorySubjectSheet'),
-        Route::get('category-sheets/{category_id}', 'categorySheet'),
-        Route::get('subject-sheets/{subject_id}', 'subjectSheet'),
         Route::get('sheets', 'index'),
+        Route::get('sheet-detail/{id}', 'detail'),
         Route::get('sheet-categories', 'category'),
+        Route::get('subject-sheets/{subject_id}', 'subjectSheet'),
+        Route::get('category-sheets/{category_id}', 'categorySheet'),
         Route::get('sheet-sub-categories/{category_id}', 'subCategory'),
-        Route::get('sheet-detail', 'detail'),
-        Route::get('premium-sheet-categories/{category_id}', 'premiumSubCategory'),
+        Route::get('sheet-premium-categories/{category_id}', 'premiumSubCategory'),
+        Route::get('category-subject-sheets/{category_id}/{subject_id}', 'categorySubjectSheet'),
     ]),
 ]);
